0cf4d6ce867e01f9c2fe2ba3faaede1e30a197c1
jeff <jeffpeng2012@gmail.com>
2025-02-13 06:23:49 +0800

    temp

--- src/components/AppBarComponent.vue
+++ src/components/AppBarComponent.vue
@@ -8,26 +8,60 @@
         <v-col class="text-center">
           <v-text-field v-model="searchQuery" single-line hide-details clearable label="搜尋" variant="outlined"
             class="short-search" @keyup.enter="emitSearchQuery" @click:clear="clearMessage"
-            @input="debounceEmitSearchQuery" autofocus >
+            @input="debounceEmitSearchQuery" autofocus>
             <template v-slot:append>
               <v-icon @click="ascendingBind = !ascendingBind"
                 :icon="ascendingBind ? mdiSortVariant : mdiSortReverseVariant">
               </v-icon>
             </template>
             <template v-slot:append-inner>
-              <v-icon :icon="mdiTune">
-                <v-dialog>
-                  <v-card>
-                    <v-select label="角色篩選" class="" variant="outlined" multiple :items="characters"
-                      style="height: 56px;">
-                      <!-- <template v-slot:selection="{ item }">
-                        <v-chip v-if="item.length < 3" v-text="item.length + ' 個角色'"></v-chip>
-                        <v-chip v-else v-text="item.length + ' 個角色'"></v-chip>
-                      </template> -->
-                    </v-select>
-                  </v-card>
-                </v-dialog>
-              </v-icon>
+              <v-dialog style="min-height: 600px;">
+                <v-card class="d-flex flex-column align-center justify-center font-weight-bold">
+                  <br />
+                  <v-btn class="text-center" @click="">
+                    清除篩選
+                  </v-btn>
+                  <!-- <span class="d-flex align-center">MyGO</span> -->
+
+                  <span class="align-center">MyGO</span>
+                  <v-select class="ma-5" label="角色篩選" chips variant="outlined" multiple :items="[1, 2, 3, 4, 5]"
+                    style="min-width:50%">
+                  </v-select>
+                  <!-- <v-row class="text-center justify-center">
+                    <v-col cols="auto" v-for="i in 13">
+                      <v-chip>
+                        {{ i }}
+                      </v-chip>
+                    </v-col>
+                  </v-row> -->
+
+                  <br />
+                  <span class="align-center">Ave Mujica</span>
+                  <v-select v-model="select" class="ma-5" label="角色篩選" variant="outlined" multiple
+                    :items="Array.from({ length: 13 }, (_, n) => n + 1)" style="min-width:50%">
+
+                    <template v-slot:selection="{ item, index }">
+                      <v-chip v-if="index < 2">
+                        <span>{{ item.title }}</span>
+                      </v-chip>
+                      <span v-if="index === 2" class="text-grey text-caption align-self-center">
+                        (+{{ select.length - 2 }} others)
+                      </span>
+                    </template>
+
+                  </v-select>
+
+                  <br />
+                  <span class="align-center">角色篩選</span>
+                  <v-select class="ma-5" label="角色篩選" chips variant="outlined" multiple :items="characters"
+                    style="min-width:50%">
+                  </v-select>
+                </v-card>
+                <template v-slot:activator="{ props: activatorProps }">
+                  <v-icon v-bind="activatorProps" :icon="mdiTune">
+                  </v-icon>
+                </template>
+              </v-dialog>
             </template>
 
           </v-text-field>
@@ -38,7 +72,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { computed, ref, watch } from "vue";
 import { mdiSortReverseVariant, mdiSortVariant, mdiTune } from "@mdi/js";
 const ascendingBind = computed({ get() { return ascending.value }, set(newValue) { ascending.value = newValue; } });
 const ascending = defineModel('ascending', { type: Boolean, required: true });
@@ -46,7 +80,11 @@ const emit = defineEmits(['update:searchQuery']);
 const searchQuery = ref("");
 const debounceTimeout = ref<number>();
 const characters = ['高松燈', '千早愛音', '要樂奈', '長崎爽世', '椎名立希', '三角初華', '豐川祥子', '八幡海鈴', '祐天寺若麥', '若葉睦'];
+const select = ref([]);
 
+watch(() => select.value, (newValue) => {
+  console.log(newValue);
+});
 
 const debounceEmitSearchQuery = () => {
   clearTimeout(debounceTimeout.value);
