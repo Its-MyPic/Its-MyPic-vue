887122aae5c1f903541c2d1d4209f89010aae685
jeffpeng3 <jeffpeng3@users.noreply.github.com>
2025-04-13 16:37:17 +0000

    deploy: 6fba22cd30afc601cd4033a25b60bde406c79591

--- assets/@unhead-DQR5zzVu.js
+++
@@ -1,1 +0,0 @@
-import{c as o,a as t,r as a}from"./unhead-NLvR5cop.js";import{n as i}from"./@vue-WVfTJAwV.js";const s="usehead";function l(e){return{install(r){r.config.globalProperties.$unhead=e,r.config.globalProperties.$head=e,r.provide(s,e)}}.install}function u(e={}){const n=o({domOptions:{render:t(()=>a(n),i)},...e});return n.install=l(n),n}export{u as c};
